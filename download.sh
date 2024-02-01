c35e406e86d0095ceb165f633365dc2d65c7ce01
sudorook <daemon@nullcodon.com>
2024-02-01 17:44:23 -0600

    add dl script

---
+++ download.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+set -euo pipefail
+
+PREFIX="https://www.forgottenbooks.com/en"
+
+function get_free_book_url {
+  curl -s "${PREFIX}" |
+    gzip -d |
+    sed -n 's|.*</a></span></div><a href="\([A-Za-z0-9_\/]\+\)"><div class="book".*|\1|p'
+}
+
+BOOK="$(get_free_book_url)"
+
+echo "${BOOK@Q}"
+curl -s -O "${PREFIX}/download/${BOOK##*/}.pdf"
