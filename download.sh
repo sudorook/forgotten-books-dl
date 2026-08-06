77b93ec21f47593544bdc3210372bee7eb875286
sudorook <daemon@nullcodon.com>
2026-08-06 11:46:31 -0600

    drip redundant gzip pipe and have curl decompress directly

--- download.sh
+++ download.sh
@@ -22,9 +22,8 @@ set -euo pipefail
 PREFIX="https://www.forgottenbooks.com/en"
 
 function get_free_book_url {
-  curl -s "${PREFIX}" |
-    gzip -d |
-    sed -n 's|.*</a></span></div><a href="\([A-Za-z0-9_\/]\+\)"><div class="book".*|\1|p'
+  curl -sL --compressed "${PREFIX}" |
+    sed -nE 's|.*</a></span></div><a href="([A-Za-z0-9_/]+)"><div class="book".*|\1|p'
 }
 
 BOOK="$(get_free_book_url)"
